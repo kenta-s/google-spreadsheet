fb12299a4650c37dce06e46128ba3f037635d7d5
kenta-s <knt01222@gmail.com>
2017-11-18 08:57:45 +0900

    Print values

--- README.md
+++ README.md
@@ -21,5 +21,5 @@ $ stack exec spreadsheet-reader spreadsheetId range
 
 example
 ```
-$ stack exec spreadsheet-reader 1-Ego70qG71GJjo3-NdAK8_8Xzxdot3qjz5dkYTenZ6k B2:C100
+$ stack exec spreadsheet-reader 1-Ego70qG71GJjo3-NdAK8_8Xzxdot3qjz5dkYTenZ6k B2:D100
 ```
--- src/Main.hs
+++ src/Main.hs
@@ -54,5 +54,19 @@ main = do
   let sheetId = Prelude.head args
       ranges = Prelude.last args
   ranges' <- fetchValueRanges sheetId ranges
-  putStrLn $ Data.Text.unpack $ Prelude.head $ Prelude.tail $ Prelude.head $ values $ Prelude.head $ ranges'
-  print $ Prelude.head $ Prelude.tail $ Prelude.head $ values $ Prelude.head $ ranges'
+  let vs = values $ Prelude.head ranges'
+  printAllValues vs
+
+printAllValues :: [[Text]] -> IO ()
+printAllValues [x] = printValue x
+printAllValues (x:xs) = do
+  let x = Prelude.head xs
+  printValue x
+  printAllValues xs
+
+printValue :: [Text] -> IO ()
+printValue x = do
+  let tl = Prelude.tail x
+  putStrLn $ Data.Text.unpack $ Prelude.head x
+  putStrLn $ Data.Text.unpack $ Prelude.head tl
+  putStrLn $ Data.Text.unpack $ Prelude.last tl
