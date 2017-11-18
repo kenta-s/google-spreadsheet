e8a39269407ed48c47a752506899b361dd0742f6
kenta-s <knt01222@gmail.com>
2017-11-18 13:38:09 +0900

    project name space: google-drive

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Lib
-import Spreadsheet.Reader
-import System.Environment    (getEnv, getArgs)
-import Data.Text
+import Web.Spock
+import Web.Spock.Config
+
+import Control.Monad.Trans
+import Data.Monoid
+import Data.IORef
+import qualified Data.Text as T
+
+data MySession = EmptySession
+data MyAppState = DummyAppState (IORef Int)
 
 main :: IO ()
-main = do
-  args <- getArgs
-  let sheetId = Prelude.head args
-      ranges = Prelude.last args
-  ranges' <- fetchValueRanges sheetId ranges
-  printAllValues ranges'
+main =
+    do ref <- newIORef 0
+       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+       runSpock 8080 (spock spockCfg app)
 
-printAllValues :: [[Text]] -> IO ()
-printAllValues [x] = printValue x
-printAllValues (x:xs) = do
-  let x = Prelude.head xs
-  printValue x
-  printAllValues xs
-
-printValue :: [Text] -> IO ()
-printValue x = do
-  let tl = Prelude.tail x
-  putStrLn $ Data.Text.unpack $ Prelude.head x
-  putStrLn $ Data.Text.unpack $ Prelude.head tl
-  putStrLn $ Data.Text.unpack $ Prelude.last tl
+app :: SpockM () MySession MyAppState ()
+app =
+    do get root $
+           text "Hello World!"
+       get ("hello" <//> var) $ \name ->
+           do (DummyAppState ref) <- getState
+              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
