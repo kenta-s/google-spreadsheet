d740f78e2de0c99a31141f0463dd4c35b35e5520
kenta-s <knt01222@gmail.com>
2017-11-18 10:22:48 +0900

    Change stack template

--- src/Spreadsheet/Reader.hs
+++ src/Spreadsheet/Reader.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
 
-module Main where
+module Spreadsheet.Reader (fetchValueRanges, values) where
 
 import           Data.Aeson
 import           GHC.Generics
@@ -47,26 +47,3 @@ fetchValueRanges sheetId ranges = do
   let req' = HS.setRequestHeader "Authorization" [S8.pack token'] req
   res <- HS.httpJSON req'
   return $ valueRanges (HS.getResponseBody res :: ValueRanges)
-
-main :: IO ()
-main = do
-  args <- getArgs
-  let sheetId = Prelude.head args
-      ranges = Prelude.last args
-  ranges' <- fetchValueRanges sheetId ranges
-  let vs = values $ Prelude.head ranges'
-  printAllValues vs
-
-printAllValues :: [[Text]] -> IO ()
-printAllValues [x] = printValue x
-printAllValues (x:xs) = do
-  let x = Prelude.head xs
-  printValue x
-  printAllValues xs
-
-printValue :: [Text] -> IO ()
-printValue x = do
-  let tl = Prelude.tail x
-  putStrLn $ Data.Text.unpack $ Prelude.head x
-  putStrLn $ Data.Text.unpack $ Prelude.head tl
-  putStrLn $ Data.Text.unpack $ Prelude.last tl
