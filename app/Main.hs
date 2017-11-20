b4552a229b9fc173722ed920ba6aa022b3286c93
kenta-s <knt01222@gmail.com>
2017-11-21 08:12:37 +0900

    Move repository

--- app/Main.hs
+++ app/Main.hs
@@ -1,44 +1,28 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
-import Web.Spock
-import Web.Spock.Config
-
-import Control.Monad.Trans
-import Data.Monoid
-import Data.IORef
-import qualified Data.Text as T
+import Lib
 import Spreadsheet.Reader
-import System.Environment  (getEnv, getArgs)
-import Data.Aeson          hiding(json)
-import GHC.Generics
-
-data MySession = EmptySession
-data MyAppState = DummyAppState (IORef Int)
-
-data SpreadsheetResult = SpreadsheetResult {
-  sheetresult :: [[T.Text]]
-} deriving (Generic, Show)
-instance ToJSON SpreadsheetResult
-instance FromJSON SpreadsheetResult
+import System.Environment    (getEnv, getArgs)
+import Data.Text
 
 main :: IO ()
 main = do
-    ref <- newIORef 0
-    spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
-    runSpock 8081 (spock spockCfg app)
+  args <- getArgs
+  let sheetId = Prelude.head args
+      ranges = Prelude.last args
+  ranges' <- fetchValueRanges sheetId ranges
+  printAllValues ranges'
 
-app :: SpockM () MySession MyAppState ()
-app =
-    do get root $
-           text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+printAllValues :: [[Text]] -> IO ()
+printAllValues [x] = printValue x
+printAllValues (x:xs) = do
+  let x = Prelude.head xs
+  printValue x
+  printAllValues xs
 
-       get ("spreadsheet" <//> var <//> var) $ \sheetId range ->
-           do (DummyAppState ref) <- getState
-              results <- liftIO $ fetchValueRanges (T.unpack sheetId) (T.unpack range)
-              json results
+printValue :: [Text] -> IO ()
+printValue x = do
+  let tl = Prelude.tail x
+  putStrLn $ Data.Text.unpack $ Prelude.head x
+  putStrLn $ Data.Text.unpack $ Prelude.head tl
+  putStrLn $ Data.Text.unpack $ Prelude.last tl
